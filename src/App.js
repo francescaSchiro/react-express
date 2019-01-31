8dd69f06f16b404a02ae97af9169bea4d51dd764
Schiro Francesca <francesca.schiro@sisal.it>
2019-01-31 10:15:19 +0100

    ADD server to existing project folder | npm start cmd starts also server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+
+const endpoint = 'http://localhost:8000/upload';
+// const getEndpoint = 'http://localhost:8000/files';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedFile: null,
+      loaded: 0,
+    }
+  }
+  
+  // get uploaded file name list in componentDidMount()
+  // componentDidMount(){
+  //   axios
+  //     .get(getEndpoint)
+  //       //handle success
+  //     .then(res => console.log(res))
+  //       // handle error
+  //     .catch(err => console.log(err))
+  //       // always executed
+  // }
+  
+  handleselectedFile = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  handleUpload = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile, this.state.selectedFile.name)
+
+    axios
+      .post(endpoint, data, {
+        onUploadProgress: ProgressEvent => {
+          this.setState({
+            loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
+          })
+        },
+      })
+      .then(res => {
+        console.log(res.statusText)
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="file" name="" id="" onChange={this.handleselectedFile} />
+        <button onClick={this.handleUpload}>Upload</button>
+        <div> {Math.round(this.state.loaded, 2)} %</div>
       </div>
-    );
+    )
   }
 }
 
